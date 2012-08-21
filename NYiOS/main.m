82b0126617035c72d125661524e582e7b58bc074
danbretl <danbretl@gmail.com>
2012-08-21 16:28:53 -0400

    Initial Commit

---
+++ NYiOS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NYiOS
+//
+//  Created by Dan Bretl on 8/21/12.
+//  Copyright (c) 2012 Dan Bretl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
